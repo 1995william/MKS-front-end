34dfbae32a2507e3da8216a5fe2cfdbd013fa1bd
Will <rwps1234@gmail.com>
2022-07-09 23:11:34 -0300

    Criando componente Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer,Logo } from "./styled";
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <Logo>
+        <h1>MKS</h1>
+        <p>Sistemas</p>
+      </Logo>
+    </HeaderContainer>
+  );
+};
