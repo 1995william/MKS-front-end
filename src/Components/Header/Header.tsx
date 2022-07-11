abbdd2eb5e21e74d48074165f792022c364e503a
Will <rwps1234@gmail.com>
2022-07-11 01:25:07 -0300

    importando componente Button

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,8 @@
-import { HeaderContainer,Logo } from "./styled";
+import React from "react";
+import { Button } from "../Button/Button";
+import { HeaderContainer, Logo } from "./styled";
+import carrinho from "../../assets/imgs/carrinho.svg";
+
 export const Header = () => {
   return (
     <HeaderContainer>
@@ -6,6 +10,12 @@ export const Header = () => {
         <h1>MKS</h1>
         <p>Sistemas</p>
       </Logo>
+
+      <Button
+        icone={carrinho}
+        texto={0}
+      />
     </HeaderContainer>
   );
 };
+
