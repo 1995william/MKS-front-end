c8161674d8db22f8246249b678d2351216de48db
Will <rwps1234@gmail.com>
2022-07-12 11:48:51 -0300

    ajustando tamanho máximo

--- src/Components/Produtos/styled.ts
+++ src/Components/Produtos/styled.ts
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 
 export const ProdutosContainer = styled.section`
-  position: absolute;
-  min-width: 938px;
-  min-height: 605px;
-  left: 247px;
-  top: 217px;
+  max-width: 938px;
+  max-height: 605px;
+  margin: 0 auto;
+  padding: 0 5px;
   display: grid;
-  grid-template-columns: repeat(4,1fr);
-  grid-template-rows: repeat(2,1fr);
+  gap: 31px 15px;
+  grid-template-columns: repeat(auto-fill, minmax(218px, 1fr));
+  grid-template-rows: repeat(auto-fill, minmax(285px, 1fr));
+
   div {
     max-width: 218px;
     max-height: 285px;
+  }
+  @media (max-width: 960px) {
+    max-height: none;
+    justify-items: center;
   }
 `;
