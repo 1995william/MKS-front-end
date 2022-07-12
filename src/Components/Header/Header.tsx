ea1d751dd637eae289963249fb0ed9cc6ff4c79e
Will <rwps1234@gmail.com>
2022-07-12 11:31:57 -0300

    adicionando useState

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import { Button } from "../Button/Button";
 import { HeaderContainer, Logo } from "./styled";
 import carrinho from "../../assets/imgs/carrinho.svg";
+import { MenuCompras } from "../MenuCompras/MenuCompras";
 
 export const Header = () => {
+
+  const [menuOpen,setMenuOpen] = useState(false);
+ 
+ 
   return (
+    <>
+      {/* <MenuCompras open={menuOpen} /> */}
     <HeaderContainer>
       <Logo>
         <h1>MKS</h1>
@@ -14,8 +21,10 @@ export const Header = () => {
       <Button
         icone={carrinho}
         texto={0}
-      />
+        onClick={()=>setMenuOpen(!menuOpen)}
+        />
     </HeaderContainer>
+        </>
   );
 };
 
