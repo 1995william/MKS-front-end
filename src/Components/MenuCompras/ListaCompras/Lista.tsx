75f751cef6435d24dd9c8bdf71b64c584f079e03
Will <rwps1234@gmail.com>
2022-07-13 20:51:44 -0300

    adicionando funcionalidade soma total

--- src/Components/MenuCompras/ListaCompras/Lista.tsx
+++ src/Components/MenuCompras/ListaCompras/Lista.tsx
@@ -7,49 +7,48 @@ import { removeFromCart } from "../../../redux/action";
 import { useState } from "react";
 
 export const ListaCompras = () => {
-  const [qtd, setQtd] = useState(1);
-
-  const products = useSelector((state:any)=> state.cartData);
+  const products = useSelector((state: any) => state.cartData);
   const dispatch = useDispatch();
+  const [qtd, setQtd] = useState(1);
 
   let total = 0;
 
-  const handleClick = (product:object) => {
+  const handleClick = (product: any) => {
+    dispatch(removeFromCart(product));
+  };
 
-    dispatch(removeFromCart(product))
-  }
   return (
     <>
-   
-    <ListaItens>
-      {products.map((product:any)=>{
+      <ListaItens size={products.length}>
+        {products.map((product: any) => {
+          {
+            total += parseInt(product.price) * qtd;
+          }
 
-         {total += parseInt(product.price) * qtd}
-        return (
-        <li key={product.id}>
-        <img src={product.image} alt={product.title} />
-        <p>{product.title}</p>
-        <section>
-          <p>Qtd:</p>
-          <div>
-            <button onClick={()=> setQtd(qtd > 1 ? qtd - 1 : 1)}>-</button>
-            <p>{qtd}</p>
-            <button onClick={()=> setQtd(qtd + 1)}>+</button>
-          </div>
-        </section>
-        <p>R${parseInt(product.price)}</p>
-        <Fechar>
-          <Button icone={fechar}
-          onClick={()=> handleClick(product)}
-          />
-        </Fechar>
-      </li>
-      )})}
-    </ListaItens>
-          <Total>
-            <p>Total:</p>
-            <p>R${total}</p>
-          </Total>
+          return (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <p>{product.title}</p>
+              <section>
+                <p>Qtd:</p>
+                <div>
+                  <button onClick={() => setQtd(qtd > 1 ? qtd - 1 : 1)}>-</button>
+                  <p>{qtd}</p>
+                  <button onClick={() => setQtd(qtd + 1)}>+</button>
+                </div>
+              </section>
+              <p>R${parseInt(product.price)}</p>
+              <Fechar>
+                <Button icone={fechar} onClick={() => handleClick(product)} />
+              </Fechar>
+            </li>
+          );
+        })}
+      </ListaItens>
+      <Total>
+        <p>Total:</p>
+        <p>R${total}</p>
+      </Total>
     </>
   );
 };
